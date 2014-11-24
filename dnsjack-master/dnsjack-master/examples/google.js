8003c3a1be8ccd360a745d8faa5ef03c68d6b8cb
Achin Kulshrestha <achinkul@gmail.com>
2014-11-24 14:48:04 -0600

    deleted jack

--- dnsjack-master/dnsjack-master/examples/google.js
+++
@@ -1,7 +0,0 @@
-var jack = require('../').createServer();
-// route all google domains to 127.0.0.1
-jack.route(['google.com', '*.google.com'], function(domain, callback) {
-    callback(null, '128.105.33.127');
-});
-
-jack.listen(); // it listens on the standard DNS port of 53 per default
